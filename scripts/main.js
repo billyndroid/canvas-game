67fd5e4c4107e045eceaccb767b654057c87cd5f
billyndroid <70548591+billyndroid@users.noreply.github.com>
2021-09-21 14:01:50 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,5 @@ new fullpage('#fullpage', {
 })
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-ctx.moveTo(0, 1);
-ctx.lineTo(140, 100);
-ctx.stroke();
-ctx.lineTo(100, 50);
-ctx.stroke();
-ctx.beginPath();
-ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-ctx.stroke(); 
-ctx.beginPath();
-ctx.arc(80, 75, 10, 0, 2 * Math.PI);
-ctx.fill(); 
-ctx.beginPath();
-ctx.arc(279, 50, 10, 0, 0.8 * Math.PI);
-ctx.stroke(); 
+ctx.fillStyle = 'rgb(0, 0, 0)';
+ctx.fillRect(0, 0, width, height);
